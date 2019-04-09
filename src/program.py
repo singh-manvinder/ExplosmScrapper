8a750712ade0841226acb05d61bd1d7c57985680
Manvinder Singh <manvinder.singh60@gmail.com>
2019-04-09 21:38:49 +0530

    Completed Image download

--- src/program.py
+++ src/program.py
@@ -1,8 +1,10 @@
 import requests
+import datetime
 from bs4 import BeautifulSoup
 
-site_url = 'http://explosm.net/comics/5212/'
+site_url = 'http://explosm.net/'
 image_id = 'main-comic'
+dest_path = 'E:\\comic'
 
 def get_content():
     response = requests.get(site_url)
@@ -14,7 +16,21 @@ def get_content():
 def get_image_url(html):
     soup = BeautifulSoup(html, 'html.parser')
     tag = soup.find('img', {'id' : image_id})
-    return tag['src']
+    return tag['src'][2::]
 
-html  = get_content()
-print(get_image_url(html))
+def download_image(url):
+    response = requests.get(f'http://{url}')
+    if response.status_code != 200:
+        return
+        
+    save_to_file(response.content)
+
+def save_to_file(content):
+    now = datetime.datetime.now().strftime("%Y-%m-%d")
+    with open(f'{dest_path}\\{now}.png', 'wb') as f:
+        f.write(content)
+
+if __name__ == '__main__':
+    html  = get_content()
+    url = get_image_url(html)
+    download_image(url)
